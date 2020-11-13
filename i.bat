d8438f1a4a1aee687e2e4fde8ef26232580fcd6a
Fabian Weller <fabianweller7@gmail.com>
2020-11-13 21:48:07 +0100

    translated specialtext; added missing file

---
+++ i.bat
@@ -0,0 +1,39 @@
+@echo off
+@echo ---------------------------------------------------------
+@echo Converting non-script text files to data files
+@echo ---------------------------------------------------------
+@echo.
+@textconv
+@echo.
+@echo.
+@echo ---------------------------------------------------------
+@echo Copying base ROM (mother3.gba) to new test ROM (test.gba)
+@echo ---------------------------------------------------------
+@echo.
+@copy mother3j.gba test.gba
+@echo.
+@echo.
+@echo ---------------------------------------------------------
+@echo Converting audio .snd files to data files
+@echo ---------------------------------------------------------
+@echo.
+@soundconv readysetgo.snd lookoverthere_eng.snd
+@echo.
+@echo.
+@echo ---------------------------------------------------------
+@echo Creating pre-welded cast of characters & sleep mode text
+@echo ---------------------------------------------------------
+@echo.
+@m3preweld
+@echo.
+@echo.
+@echo ---------------------------------------------------------
+@echo Compiling .asm files and inserting all new data files
+@echo ---------------------------------------------------------
+@echo.
+@xkas test.gba m3hack.asm
+@echo.
+@echo.
+@echo COMPLETE!
+@echo.
+wait
